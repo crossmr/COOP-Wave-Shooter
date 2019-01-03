d7755f222a63407297b50c849f9fee7befcedc6e
Matthew <crossmr@gmail.com>
2019-01-03 12:51:08 +0900

    Implement basic waves, currently spawns a new wave every 2 seconds and increases amount of bots each time.

--- Source/TLMPSection3/Public/SGameMode.h
+++ Source/TLMPSection3/Public/SGameMode.h
@@ -15,6 +15,38 @@ class TLMPSECTION3_API ASGameMode : public AGameModeBase
 	GENERATED_BODY()
 	
 	
+protected:
+
+	//This allows blueprint to spawn a single bot
+	UFUNCTION(BlueprintImplementableEvent, Category = "GameMode")
+	void SpawnNewBot();
 	
+	//start spawning bots for each wave
+	void StartWave();
+
+	void SpawnBotTimeElapsed();
+
+	//stop spawning bots
+	void EndWave();
+
+	//Set Time for next wave
+	void PrepareForNextWave();
+
+	FTimerHandle TimeHandle_BotSpawner;
+
+	//How many bots to spawn in current wave
+	int32 NrOfBotsToSpawn;
+
+	int32 WaveCount; 
+
+	UPROPERTY(EditDefaultsOnly, Category = "GameMode")
+	float TimeBetweenWaves;
+
+public:
+
+	ASGameMode();
+
+	virtual void StartPlay() override;
 	
+
 };
